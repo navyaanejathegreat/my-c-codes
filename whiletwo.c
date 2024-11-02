2889d1b463b29f98020463abb52593f35713af3f
Navya Aneja <185716508+navyaanejathegreat@users.noreply.github.com>
2024-11-02 23:33:53 +0530

    Add files via upload

---
+++ whiletwo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int i=1;
+    int n;
+    printf("enter any integer:");
+    scanf("%d",&n);
+    while (i<=n){
+        printf("%d\n",i);
+        i++;
+    }
+return 0;
+}
